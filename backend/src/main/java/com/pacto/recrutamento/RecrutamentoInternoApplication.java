213ae7429a848351966f905a307f2ffa28fe714a
pedro-haugusto_SemParar <pedro.haugusto@corpay.com.br>
2026-08-05 18:15:34 -0300

    init: criando estrutura inicial do projeto

---
+++ backend/src/main/java/com/pacto/recrutamento/RecrutamentoInternoApplication.java
@@ -0,0 +1,12 @@
+package com.pacto.recrutamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrutamentoInternoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecrutamentoInternoApplication.class, args);
+    }
+}
