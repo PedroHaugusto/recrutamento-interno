9bc3677f45e6914e7390eb3026c462c9c1c6f654
pedro-haugusto_SemParar <pedro.haugusto@corpay.com.br>
2026-08-05 22:41:53 -0300

    feat: adiciona entidades JPA e repositories (Usuario, Vaga, Candidatura, Notificacao)

---
+++ backend/src/main/java/com/pacto/recrutamento/model/Notificacao.java
@@ -0,0 +1,46 @@
+package com.pacto.recrutamento.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notificacao")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Notificacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    @Column(nullable = false, length = 255)
+    private String mensagem;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Boolean lida = false;
+
+    @CreationTimestamp
+    @Column(name = "data_criacao", nullable = false, updatable = false)
+    private LocalDateTime dataCriacao;
+}
---
+++ backend/src/main/java/com/pacto/recrutamento/model/Usuario.java
@@ -0,0 +1,48 @@
+package com.pacto.recrutamento.model;
+
+import com.pacto.recrutamento.model.enums.RoleUsuario;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(name = "senha_hash", nullable = false)
+    private String senhaHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, columnDefinition = "role_usuario")
+    private RoleUsuario role;
+
+    @CreationTimestamp
+    @Column(name = "data_criacao", nullable = false, updatable = false)
+    private LocalDateTime dataCriacao;
+}
